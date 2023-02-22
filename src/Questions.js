3374ca4c72dc837cf28037a4df81b401aab4ab3f
feindreh <119032218+feindreh@users.noreply.github.com>
2023-02-22 21:24:58 +0100

    changes for space

--- src/Questions.js
+++ src/Questions.js
@@ -33,12 +33,12 @@ const QuestionStack = () => {
     6: newQuestion("Francis hat an welchem Ort jemanden behindert gewrestelt","in Heidis Garten","in Dirks Keller","auf Lauras Balkon","vor Lucas Wohnung"),
     7 : newQuestion('Keine der "Lugert`s" ist',"Sophia","Katharina","Annalena","Johannah"),
     8 : newQuestion("Vanessa kommt aus","dem Odenwald","der Pfalz","Hessen","Baden"),
-    9 : newQuestion("Felix hat ANGEBLICH!!! an Halloween","Kindern seine Eier gezeigt","Kinder mit Eiern beworfen","Mit einer Pferdemaske Kinder erschreckt","einem Kind die Süßigkeiten geklaut"),
+    9 : newQuestion("Felix hat ANGEBLICH!!! an Halloween","Kindern seine Eier gezeigt","Kinder mit Eiern beworfen","Mit Pferdemaske Kinder erschreckt","einem Kind die Süßigkeiten geklaut"),
     10 : newQuestion("Steven`s Wohnsitz ist in...","Seckenheim","Ilvesheim","Ladenburg","Feudenheim"),
 
 
     11: newQuestion("Steven und Laura waren zusammen in einer Klasse bis zum Ende der ... Klasse","11","10","12","13"),
-    12: newQuestion("Fabi und Danny haben sich zum ersten mal gesehen","in einer Vorlesung","bei freeletics","auf einer Wohnheimparty","Sie sind gleichzeitig aus dem Club geflogen"),
+    12: newQuestion("Fabi und Danny haben sich zum ersten mal gesehen","in einer Vorlesung","bei freeletics","auf einer Wohnheimparty","gleichzeitig aus dem Club geflogen"),
     13: newQuestion("Lucas machte sein Abitur","an der IGMH","am Lessing Gymnasium","am LFG","am KFG"),
     14: newQuestion("Katharina zieht am Samstag (alles Heidelberger Stadtteile) nach","Neuenheim","Weststadt","sie bleibt in der Altstadt","Bergheim"),
     15: newQuestion("Der Horsecockmen hat zwischen den Beinen einen","Vorschlaghammer","Abrisskeule","Morgenstern","Zweihandschwert")
