b5ad9ddd15faf1a8182e0404c62353596483d3a5
feindreh <119032218+feindreh@users.noreply.github.com>
2023-03-06 18:09:47 +0100

    add routing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 
 import React,{ Component } from "react";
+import { Routes, Route, Link } from "react-router-dom";
 
-// import Header from "./components/header";
-import Quizz from "./components/quizz"
+import Home from "./components/home";
+import Quizz from "./components/quizz";
+import MakeQuizz from "./components/makeQuizz";
 
-class App extends Component {
-    constructor(){
-        super()
-    }
 
-    render(){
-        return(
-          <div>
-            {/* <Header/> */}
-            <Quizz/>
-          </div>
-        )
-    }
+function App(){
+  return(
+    <Routes>
+      <Route path="/" element ={<Home/>}/>
+      <Route path="/play" element={<Quizz/>}/>
+      <Route path="/pick" element={<div>Pick</div>}/>
+      <Route path="/create" element ={<MakeQuizz/>}/>
+    </Routes>
+  )
 }
 
 export default App
