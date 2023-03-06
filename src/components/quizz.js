d67b386b9d6ff6efe99b1ca4b0f0037f5346fce0
feindreh <119032218+feindreh@users.noreply.github.com>
2023-03-06 18:10:36 +0100

    add name

--- src/components/quizz.js
+++ src/components/quizz.js
@@ -2,7 +2,6 @@ import React,{ Component } from "react";
 import QuestionStack from "../Questions";
 import NextButton from "./nextbutton";
 import Answer from "./answers";
-import Cashbar from "./cashbar";
 
 class Quizz extends Component {
     constructor(){
@@ -11,6 +10,7 @@ class Quizz extends Component {
             count : 0,
             questions : QuestionStack(),
             pickedAnswer:null,
+            name:QuestionStack().name,
         }
         this.rightAnswer = this.rightAnswer.bind(this)
         this.wrongAnswer = this.wrongAnswer.bind(this)
@@ -44,22 +44,26 @@ class Quizz extends Component {
     }
 
     render(){
-        const {count,pickedAnswer} = this.state
+        const {count,pickedAnswer,name} = this.state
+
         if(this.state.questions[count] === undefined){return <div id="winner"> </div>}
+
         const questions = this.state.questions[count].Answers
         let id = 0
             return(
-              <div id="quizz">
-                <div className="question">{this.state.questions[count].question}</div>
-                <div className="answers">
-                    {questions.map((answer)=>{
-                        id++
-                        return <Answer letter={getLetter(id)}key={id} answer={answer} good={this.state.questions[count].rightAnswer} pickedAnswer={pickedAnswer} callback={this.AnswerPicked}/>
-                    })}
-                </div>
-                <Cashbar count={count}/>
-                <NextButton answer={pickedAnswer} right={this.rightAnswer} wrong={this.wrongAnswer}/>
-              </div>  
+              <div>
+                  <div id="quizzName">{name}</div>
+                  <div id="quizz">
+                    <div className="question">{this.state.questions[count].question}</div>
+                    <div className="answers">
+                        {questions.map((answer)=>{
+                            id++
+                            return <Answer letter={getLetter(id)}key={id} answer={answer} good={this.state.questions[count].rightAnswer} pickedAnswer={pickedAnswer} callback={this.AnswerPicked}/>
+                        })}
+                    </div>
+                    <NextButton answer={pickedAnswer} right={this.rightAnswer} wrong={this.wrongAnswer}/>
+                  </div>
+              </div>
             )
      
         
