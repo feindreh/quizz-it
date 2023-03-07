13349f2ee90943a30d049e582996181817708890
feindreh <119032218+feindreh@users.noreply.github.com>
2023-03-07 19:14:53 +0100

    changed addDoc to setDoc

--- src/firebase/firestore/firestore.js
+++ src/firebase/firestore/firestore.js
@@ -1,4 +1,4 @@
-import {getFirestore,collection, addDoc,getDocs } from "firebase/firestore";
+import {getFirestore,collection, addDoc,getDocs,doc ,setDoc} from "firebase/firestore";
 import uniqid from "uniqid"
 
 
@@ -7,26 +7,39 @@ import app from "../app";
 
 const db = getFirestore(app);
 
-  export default async function saveQuizz(quizz,name){
-    try{
-      console.log("save")
-      await addDoc(collection(db,"quizz"),{
-        quizz:quizz,
-        name:name,
-        id:uniqid()
-      });
-    } catch(e){
-      console.error("Error saving Quizz:",e)
-    }
-  }
-
-  export async function getQuizz(){
-    console.log("read")
-    const quizzes = await getDocs(collection(db, "quizz"));
-    
-    const quizArray = []
-    quizzes.forEach((quis) => {
-        quizArray.push(quis.data())
+export default async function saveQuizz(quizz,name,id = uniqid()){
+  try{
+    console.log("save")
+    await setDoc(doc(collection(db,"quizz"),id),{
+      quizz:quizz,
+       name:name,
+       id:uniqid()
     })
-    return quizArray
+  } catch(e){
+    console.error("Error saving Quizz:",e)
   }
+}
+
+// export default async function saveQuizz(quizz,name){
+//   try{
+//     console.log("save")
+//     await addDoc(collection(db,"quizz"),{
+//       quizz:quizz,
+//       name:name,
+//       id:uniqid()
+//     });
+//   } catch(e){
+//       console.error("Error saving Quizz:",e)
+//   }
+// }
+
+export async function getQuizz(){
+  console.log("read")
+  const quizzes = await getDocs(collection(db, "quizz"));
+
+  const quizArray = []
+  quizzes.forEach((quis) => {
+      quizArray.push(quis.data())
+  })
+  return quizArray
+}
