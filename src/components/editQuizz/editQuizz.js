d8d5a15f310f8d63a5c63cae7584fdcc6de658a4
feindreh <119032218+feindreh@users.noreply.github.com>
2023-03-07 19:12:58 +0100

    initial commit

---
+++ src/components/editQuizz/editQuizz.js
@@ -0,0 +1,11 @@
+import saveQuizz ,{getQuizz} from "../../firebase/firestore/firestore"
+import { useParams } from "react-router-dom";
+
+function EditQuizz(){
+
+    return(
+        <div>Edit Quizz</div>
+    )
+}
+
+export default EditQuizz
