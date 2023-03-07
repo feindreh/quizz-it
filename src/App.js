69817d4984d4ed9eacd629a248c029a0b0c1358d
feindreh <119032218+feindreh@users.noreply.github.com>
2023-03-07 19:13:24 +0100

    combined edit and pick in one component with props

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import { Routes, Route, Link } from "react-router-dom";
 import Home from "./components/home";
 import Quizz from "./components/quizz";
 import MakeQuizz from "./components/makeQuizz/makeQuizz";
-import Edit from "./components/edit";
 import PickQuizz from "./components/pickQuizz/pickQuizz";
+import EditQuizz from "./components/editQuizz/editQuizz";
 
 import { getQuizz } from "./firebase/firestore/firestore";
 import { useState,useEffect } from "react";
@@ -23,9 +23,10 @@ function App(){
     <Routes>
       <Route path="/" element ={<Home/>}/>
       <Route path="/play/:id" element={<Quizz quizzes={quizzes}/>}/>
-      <Route path="/pick" element={<PickQuizz quizzes={quizzes}/>}/>
       <Route path="/create" element ={<MakeQuizz/>}/>
-      <Route path="/edit" element={<Edit/>}/>
+      <Route path="/pick" element={<PickQuizz quizzes={quizzes} feature={"play"}/>}/>
+      <Route path="/edit" element={<PickQuizz quizzes={quizzes} feature={"edit"}/>}></Route>
+      <Route path="/edit/:id" element={<EditQuizz quizzes={quizzes}/>}/>
     </Routes>
   )
 }
