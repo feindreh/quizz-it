bdc61552cb84c55d74f4369000bc65f584ad97fc
feindreh <119032218+feindreh@users.noreply.github.com>
2023-03-07 19:14:40 +0100

    add function to make component more reuseable

--- src/components/pickQuizz/quizzPick.js
+++ src/components/pickQuizz/quizzPick.js
@@ -1,15 +1,30 @@
 import { Link } from "react-router-dom"
 
 function QuizzPick(props){
-    const {quizz} = props
+    const {quizz,feature} = props
+
+    const text = getText(feature)
+
     return (
         <div className = "quizzPick">
             <div className="pickWrap">
                 <div className = "quizzPick-name">{quizz.name}</div>
-                <Link to={`/play/${quizz.id}`}><button type="button">Dieses Quizz Spielen</button></Link>
+                <Link to={`/${feature}/${quizz.id}`}><button type="button">{text}</button></Link>
             </div>
         </div>
     )
+}
+
+function getText(feat){
+    switch(feat){
+        case "play":
+            return "Dieses Quizz Spielen";
+        case "edit":
+            return "Dieses Quizz bearbeiten"
+        default:
+            console.error("undefined feature")
+
+    }
 }
 
 export default QuizzPick
