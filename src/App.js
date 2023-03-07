f7b6ae0d7fa78b4a1688bec81d75ae6959ab003f
feindreh <119032218+feindreh@users.noreply.github.com>
2023-03-07 01:27:03 +0100

    add useEffect to load quizzes from database

--- src/App.js
+++ src/App.js
@@ -6,14 +6,24 @@ import Home from "./components/home";
 import Quizz from "./components/quizz";
 import MakeQuizz from "./components/makeQuizz/makeQuizz";
 import Edit from "./components/edit";
+import PickQuizz from "./components/pickQuizz/pickQuizz";
 
+import { getQuizz } from "./firebase/firestore/firestore";
+import { useState,useEffect } from "react";
 
 function App(){
+
+  const [quizzes,setQuizzes] = useState([])
+
+    useEffect(()=>{
+        getQuizz().then((resolve)=>setQuizzes(resolve))
+    },[])
+
   return(
     <Routes>
       <Route path="/" element ={<Home/>}/>
-      <Route path="/play" element={<Quizz/>}/>
-      <Route path="/pick" element={<div>Pick</div>}/>
+      <Route path="/play/:id" element={<Quizz/>}/>
+      <Route path="/pick" element={<PickQuizz quizzes={quizzes}/>}/>
       <Route path="/create" element ={<MakeQuizz/>}/>
       <Route path="/edit" element={<Edit/>}/>
     </Routes>
