dfae86f18f6caa9b46098f10638b7e5f133cf724
feindreh <119032218+feindreh@users.noreply.github.com>
2023-03-07 01:25:39 +0100

    add uniqid

--- src/firebase/firestore/firestore.js
+++ src/firebase/firestore/firestore.js
@@ -1,4 +1,7 @@
 import {getFirestore,collection, addDoc,getDocs } from "firebase/firestore";
+import uniqid from "uniqid"
+
+
 
 import app from "../app";
 
@@ -9,20 +12,21 @@ const db = getFirestore(app);
       console.log("save")
       await addDoc(collection(db,"quizz"),{
         quizz:quizz,
-        name:name
+        name:name,
+        id:uniqid()
       });
     } catch(e){
       console.error("Error saving Quizz:",e)
     }
   }
 
-//   export async function getText(){
-//     console.log("read")
-//     const messages = await getDocs(collection(db, "text"));
+  export async function getQuizz(){
+    console.log("read")
+    const quizzes = await getDocs(collection(db, "quizz"));
     
-//     const messagesArray = []
-//     messages.forEach((mes) => {
-//         messagesArray.push(mes.data())
-//     })
-//     return messagesArray
-//   }
+    const quizArray = []
+    quizzes.forEach((quis) => {
+        quizArray.push(quis.data())
+    })
+    return quizArray
+  }
