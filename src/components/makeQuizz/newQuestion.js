c384821839964d13b8d307a6807198c32b61f614
feindreh <119032218+feindreh@users.noreply.github.com>
2023-03-08 01:34:19 +0100

    add display default values

--- src/components/makeQuizz/newQuestion.js
+++ src/components/makeQuizz/newQuestion.js
@@ -9,23 +9,23 @@ function NewQuestion(props){
             <div>Frage Nummer: {number+1}</div>
             <div className="wrap">
                 <label>Frage</label>
-                <input type="text" onChange={(e)=>{Question.question = e.target.value}}></input>
+                <input type="text" defaultValue={Question.question} onChange={(e)=>{Question.question = e.target.value}}></input>
             </div>
             <div className="wrap">
                 <label>Richtige Antwort:</label>
-                <input type="text" onChange={(e)=>{Question.rightAnswer = e.target.value}}></input>
+                <input type="text" defaultValue={Question.rightAnswer} onChange={(e)=>{Question.rightAnswer = e.target.value}}></input>
             </div>
             <div className="wrap">
                 <label>Antwort 2:</label>
-                <input type="text" onChange={(e)=>{Question.answer2 = e.target.value}}></input>
+                <input type="text" defaultValue={Question.answer2} onChange={(e)=>{Question.answer2 = e.target.value}}></input>
             </div>
             <div className="wrap">
                 <label>Antwort 3:</label>
-                <input type="text" onChange={(e)=>{Question.answer3 = e.target.value}}></input>
+                <input type="text" defaultValue={Question.answer3} onChange={(e)=>{Question.answer3 = e.target.value}}></input>
             </div>
             <div className="wrap">
                 <label>Antwort 4:</label>
-                <input type="text" onChange={(e)=>{Question.answer4 = e.target.value}}></input>
+                <input type="text" defaultValue={Question.answer4} onChange={(e)=>{Question.answer4 = e.target.value}}></input>
             </div>
         </div>
     )
