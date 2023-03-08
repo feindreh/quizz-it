de7c9274d2d0b3efb8556a1784d3f7a2ab22aff1
feindreh <119032218+feindreh@users.noreply.github.com>
2023-03-08 01:34:08 +0100

    add params to be reuseable

--- src/components/makeQuizz/makeQuizz.js
+++ src/components/makeQuizz/makeQuizz.js
@@ -1,12 +1,48 @@
 import {useState} from "react"
+import { useParams } from "react-router-dom"
 import saveQuizz from "../../firebase/firestore/firestore"
 import "./makeQuizz.css"
 import NewQuestion from "./newQuestion"
 
-function MakeQuizz(){
+function MakeQuizz(props){
 
-    const [newQuizz,setNewQuizz] = useState([])
-    const [name,setName] = useState("")
+    const {id} = useParams() 
+    const {quizzes} = props
+
+    function getNewQuizz(){
+        //if no params return empty array else return quizz to edit
+        if(id===undefined){
+            return []
+        }else{
+            for(let key in quizzes){
+                if(quizzes[key].id === id){
+                    const newArray = []
+                    for(let subkey in quizzes[key].quizz){
+                        newArray.push(quizzes[key].quizz[subkey])
+                    }
+                    return newArray
+                }
+            }
+        }
+    }
+
+    function getNewName(){
+         //if no params return empty string else return name to edit
+        if(id===undefined){
+            return ""
+        }else{
+            for(let key in quizzes){
+                if(quizzes[key].id === id){
+                    return quizzes[key].name
+                }
+            }
+        }
+    }
+
+    const [newQuizz,setNewQuizz] = useState(getNewQuizz())
+    const [name,setName] = useState(getNewName())
+
+    
 
     async function saveIT(){
         const newObj = {}
@@ -14,18 +50,19 @@ function MakeQuizz(){
         for(let key in newQuizz){
             newObj[key] = newQuizz[key]
         }  
-        saveQuizz(newObj,name);
+        saveQuizz(newObj,name,id);
         setNewQuizz([]);
         setName("")
 
     }
 
+    console.log(newQuizz)
 
     return(
         <div id="quizzMaker">
             <div>makeQuizz</div>
             <button type="button" onClick={saveIT}>Speichern</button>
-            <label>Name des Quizzes</label><input type = "text" onChange={(e)=>{setName(e.target.value)}}></input>
+            <label>Name des Quizzes</label><input type = "text" defaultValue={name} onChange={(e)=>{setName(e.target.value)}}></input>
             
             {newQuizz.map((question,a)=><NewQuestion key={a} number={a} Question={question}/>)}
 
