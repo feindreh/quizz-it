c18cd269d08a83fb245a0d2b94da96b503d22942
feindreh <119032218+feindreh@users.noreply.github.com>
2023-03-09 21:13:53 +0100

    moved fetching into the commponents to avoid routing problems

--- src/App.js
+++ src/App.js
@@ -1,30 +1,21 @@
 
-import React,{ Component } from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
 import Home from "./components/home";
 import Quizz from "./components/play/quizz";
 import MakeQuizz from "./components/makeQuizz/makeQuizz";
 import PickQuizz from "./components/pickQuizz/pickQuizz";
 
-import { getQuizz } from "./firebase/firestore/firestore";
-import { useState,useEffect } from "react";
-
 function App(){
-
-  const [quizzes,setQuizzes] = useState([])
-
-    useEffect(()=>{
-        getQuizz().then((resolve)=>setQuizzes(resolve))
-    },[])
 
   return(
     <Routes>
       <Route path="/" element ={<Home/>}/>
-      <Route path="/play/:id" element={<Quizz quizzes={quizzes}/>}/>
+      <Route path="/play/:id" element={<Quizz/>}/>
       <Route path="/create" element ={<MakeQuizz/>}/>
-      <Route path="/pick" element={<PickQuizz quizzes={quizzes} feature={"play"}/>}/>
-      <Route path="/edit" element={<PickQuizz quizzes={quizzes} feature={"edit"}/>}></Route>
+      <Route path="/pick" element={<PickQuizz feature={"play"}/>}/>
+      <Route path="/edit" element={<PickQuizz feature={"edit"}/>}></Route>
       <Route path="/edit/:id" element={<MakeQuizz/>}/>
     </Routes>
   )
