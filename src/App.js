e4594013cf98ce004d33fd1d584980ccb607ef67
feindreh <119032218+feindreh@users.noreply.github.com>
2023-03-10 02:04:54 +0100

    add load functions as props to makequizz

--- src/App.js
+++ src/App.js
@@ -30,10 +30,10 @@ const [quizzes,setQuizzes] = useState([])
       <Routes>
         <Route path="/quizz-it/" element ={<Home/>}/>
         <Route path="/quizz-it/play/:id" element={<Quizz quizzes={quizzes}/>}/>
-        <Route path="/quizz-it/create" element ={<MakeQuizz quizzes={quizzes}/>}/>
-        <Route path="/quizz-it/pick" element={<PickQuizz quizzes={quizzes} feature={"play"}/>}/>
-         <Route path="/quizz-it/edit" element={<PickQuizz quizzes={quizzes} feature={"edit"}/>}></Route>
-        <Route path="/quizz-it/edit/:oldId" element={<MakeQuizz quizzes={quizzes}/>}/>
+        <Route path="/quizz-it/create" element ={<MakeQuizz quizzes={quizzes} load={load} />}/>
+        <Route path="/quizz-it/pick" element={<PickQuizz quizzes={quizzes}  feature={"play"}/>}/>
+         <Route path="/quizz-it/edit" element={<PickQuizz quizzes={quizzes}  feature={"edit"}/>}></Route>
+        <Route path="/quizz-it/edit/:oldId" element={<MakeQuizz quizzes={quizzes} load={load}/>}/>
       </Routes>
     </div>
   )
